35dbafe639cef3c943e9c4c3fa8dc09260f6d947
Julian <julian.kuypers@signature.ai>
2024-12-18 16:04:27 +0000

    update dry run condition

--- release.config.js
+++ release.config.js
@@ -1,25 +1,14 @@
 module.exports = {
-  branches: ["main"],
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "angular",
-        releaseRules: [
-          { type: "major", release: "major" },
-          { type: "minor", release: "minor" },
-          { type: "patch", release: "patch" },
-        ],
-      },
-    ],
-    "@semantic-release/changelog",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-  ],
+    branches: ['main'],
+    plugins: [
+        ["@semantic-release/commit-analyzer", {
+            "preset": "angular",
+            "releaseRules": [
+                {
+                    "release": process.env.RELEASE_TYPE || "patch"
+                }
+            ]
+        }],
+        '@semantic-release/git'
+    ]
 };
