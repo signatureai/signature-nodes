50725c02c0403174e0ffa240faa4b4d2449c3faf
Miguel Pereira <miguel.pereira@signature.ai>
2025-01-14 15:05:17 +0000

    Remove binary_mask option and update checkpoint id

--- nodes/neurochain/detectors/segmentation/u2net.py
+++ nodes/neurochain/detectors/segmentation/u2net.py
@@ -12,18 +12,17 @@ class U2NetNode:
         return {
             "required": {
                 "image": ("IMAGE",),
-                "clearml_model_id": ("STRING", {"default": "43248a75117d4f4db2d9187fea288eff"}),
-                "use_binary_mask": ("BOOLEAN", {"default": True}),
+                "clearml_model_id": ("STRING", {"default": "fefce5f3d54a479fa02c84f6fdf3b8a2"}),
             },
         }
 
-    RETURN_TYPES = ("IMAGE",)
+    RETURN_TYPES = ("MASK",)
     FUNCTION = "process"
     CATEGORY = SEGMENTATION_CAT
 
-    def process(self, image: torch.Tensor, clearml_model_id: str, use_binary_mask: bool):
-        root_dir = Path(__file__).parent.parent.parent
+    def process(self, image: torch.Tensor, clearml_model_id: str):
+        root_dir = Path(__file__).parents[6]  # TODO: replace this with an automatic method to get the root
         checkpoint_dir = root_dir / "models" / "checkpoints"
-        model = U2Net(model_id=clearml_model_id, checkpoint_dir=checkpoint_dir, use_binary_mask=use_binary_mask)
+        model = U2Net(model_id=clearml_model_id, checkpoint_dir=checkpoint_dir)
         output = model.predict(image)
         return (output,)
