758a8cc05022bea866cf9c3a3705587320bf1e2e
Afonso Miguel <afonso.miguel@signature.ai>
2025-01-27 14:21:26 +0000

    fix: fixed format of mask

--- nodes/file.py
+++ nodes/file.py
@@ -10,9 +10,11 @@ from .shared import BASE_COMFY_DIR
 
 def image_array_to_tensor(x: TensorImage):
     image = x.get_BWHC()
-    mask = torch.ones((x.shape[0], 1, x.shape[2], x.shape[3]), dtype=torch.float32)
-    if x.shape[1] == 4:
+    mask = torch.ones((x.shape[0], x.shape[2], x.shape[3], 1), dtype=torch.float32)
+
+    if x.shape[-1] == 4:
         mask = image[:, :, :, -1]
+
     return (
         image,
         mask,
