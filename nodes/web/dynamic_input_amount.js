20cd2f4f683e793691694758b2d787813be23679
Afonso Miguel <afonso.miguel@signature.ai>
2025-02-07 13:16:53 +0000

    styling: fix styling with linter

--- nodes/web/dynamic_input_amount.js
+++ nodes/web/dynamic_input_amount.js
@@ -1,8 +1,8 @@
 import { app } from "../../../scripts/app.js";
 
-const extNames = ["signature.batch_builder", "signature.list_builder", "signature.math_operator"]
+const extNames = ["signature.batch_builder", "signature.list_builder", "signature.math_operator"];
 
-const classNames = ["signature_list_builder", "signature_batch_builder", "signature_math_operator"]
+const classNames = ["signature_list_builder", "signature_batch_builder", "signature_math_operator"];
 
 function updateTypesBasedOnConnection(node) {
   // Find if there's at least one connected input among visible and hidden inputs
@@ -12,8 +12,9 @@ function updateTypesBasedOnConnection(node) {
   let typeToUse = "*";
   if (connectedInput) {
     // Get the connected node's output type
-    typeToUse = app.graph.getNodeById(app.graph.links[connectedInput.link].origin_id)
-      .outputs[app.graph.links[connectedInput.link].origin_slot].type;
+    typeToUse = app.graph.getNodeById(app.graph.links[connectedInput.link].origin_id).outputs[
+      app.graph.links[connectedInput.link].origin_slot
+    ].type;
   }
   // prevent math operator node from updating types on inputs
   if (node.comfyClass === "signature_math_operator") {
@@ -39,9 +40,8 @@ function handleNumSlots(node, widget) {
 
   for (let i = 1; i <= 10; i++) {
     // Find inputs with either numeric suffix (_1, _2...) or alphabetic name (a, b...)
-    const inputs = [...node.inputs, ...node.inputsHidden].filter((input) =>
-      input.name.endsWith(`_${i}`) || 
-      input.name === String.fromCharCode(96 + i)  // a = 97, b = 98, etc.
+    const inputs = [...node.inputs, ...node.inputsHidden].filter(
+      (input) => input.name.endsWith(`_${i}`) || input.name === String.fromCharCode(96 + i) // a = 97, b = 98, etc.
     );
 
     // Group inputs by their base name (removing the _N suffix for numeric ones)
@@ -49,9 +49,7 @@ function handleNumSlots(node, widget) {
     inputs.forEach((input) => {
       // For alphabetic inputs (a, b, c...), use the full name as base name
       // For numeric suffix inputs (test_1, test_2...), remove the suffix
-      const baseName = input.name.match(/^[a-z]$/) ? 
-        input.name : 
-        input.name.replace(/_\d+$/, "");
+      const baseName = input.name.match(/^[a-z]$/) ? input.name : input.name.replace(/_\d+$/, "");
 
       if (!inputsByName[baseName]) {
         inputsByName[baseName] = [];
@@ -62,16 +60,16 @@ function handleNumSlots(node, widget) {
     // Process each group of inputs separately
     Object.values(inputsByName).forEach((sameNameInputs) => {
       if (sameNameInputs.length === 0) return;
-  
+
       const input = sameNameInputs[0];
-  
+
       if (i <= numSlots) {
         // Move to visible inputs if not already there
         const hiddenIndex = node.inputsHidden.indexOf(input);
         const inputIndex = node.inputs.indexOf(input);
         if (hiddenIndex !== -1 && inputIndex === -1) {
           // Find the index of the "value" input
-          const valueInputIndex = node.inputs.findIndex(input => input.name === "value");
+          const valueInputIndex = node.inputs.findIndex((input) => input.name === "value");
           // Insert before "value" input if it exists, otherwise append to end
           const insertIndex = valueInputIndex !== -1 ? valueInputIndex : node.inputs.length;
           node.inputs.splice(insertIndex, 0, input);
@@ -90,7 +88,7 @@ function handleNumSlots(node, widget) {
           }
           // Clear the input value
           input.value = undefined;
-          
+
           node.inputsHidden.push(input);
           node.inputs.splice(inputIndex, 1);
         }
@@ -124,7 +122,7 @@ function widgetLogic(node, widget, isInitial = false) {
 extNames.forEach((name) => {
   const ext = {
     name: name,
-  
+
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
       const className = nodeType.comfyClass;
       if (classNames.includes(className)) {
@@ -132,20 +130,15 @@ extNames.forEach((name) => {
         if (!nodeType.prototype) {
           nodeType.prototype = {};
         }
-  
-        nodeType.prototype.onConnectionsChange = function (
-          type,
-          index,
-          connected,
-          link_info,
-        ) {
+
+        nodeType.prototype.onConnectionsChange = function (type, index, connected, link_info) {
           const slot = this.inputs[index];
           if (!slot) return;
           updateTypesBasedOnConnection(this);
         };
       }
     },
-  
+
     async nodeCreated(node) {
       const className = node.comfyClass;
       if (classNames.includes(className)) {
@@ -155,27 +148,25 @@ extNames.forEach((name) => {
           Object.defineProperty(w, "value", {
             get() {
               let valueToReturn =
-                originalDescriptor && originalDescriptor.get
-                  ? originalDescriptor.get.call(w)
-                  : widgetValue;
+                originalDescriptor && originalDescriptor.get ? originalDescriptor.get.call(w) : widgetValue;
               return valueToReturn;
             },
             set(newVal) {
               const oldVal = widgetValue;
-  
+
               if (originalDescriptor && originalDescriptor.set) {
                 originalDescriptor.set.call(w, newVal);
               } else {
                 widgetValue = newVal;
               }
-  
+
               if (oldVal !== newVal) {
                 widgetLogic(node, w);
               }
             },
           });
         }
-  
+
         const numSlotsWidget = node.widgets.find((w) => w.name === "num_slots");
         if (numSlotsWidget) {
           handleNumSlots(node, numSlotsWidget);
@@ -184,4 +175,4 @@ extNames.forEach((name) => {
     },
   };
   app.registerExtension(ext);
-})
+});
