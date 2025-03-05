ce3d854c164788f8a34d304dc80f9988fa5a1357
Afonso Miguel <afonso.miguel@signature.ai>
2025-03-05 17:22:27 +0000

    feat: create test to assure randomisation of ksampler

--- nodes/web/tests/main.js
+++ nodes/web/tests/main.js
@@ -1,4 +1,15 @@
 import { bypassNodes } from "./bypass_nodes.js";
 import { checkNodeGroupPresence } from "./check_node_group_presence.js";
+import {
+  findNodesWithRandomizedControlAfterGenerateWidget,
+  highlightNodesWithRandomizedWidgets,
+  showRandomizationWarningDialog,
+} from "./nodes_with_randomized_control_after_generate.js";
 
-export { bypassNodes, checkNodeGroupPresence };
+export {
+  bypassNodes,
+  checkNodeGroupPresence,
+  findNodesWithRandomizedControlAfterGenerateWidget,
+  highlightNodesWithRandomizedWidgets,
+  showRandomizationWarningDialog,
+};
