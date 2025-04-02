aec72de111e178594b5e78a13054dafc45c9b7ca
Marco João <marco.joao@signature.ai>
2025-04-02 13:44:42 +0100

    feat: add GetModelUrl class for fetching model URLs from backend API using Cognito authentication

---
+++ nodes/core/models/get_model_url.py
@@ -0,0 +1,63 @@
+import json
+
+import boto3
+import requests
+
+from ...categories import PLATFORM_IO_CAT
+
+
+class GetModelUrl:
+    @classmethod
+    def INPUT_TYPES(cls):  # type: ignore
+        return {
+            "required": {
+                "model_uuid": ("STRING", {"forceInput": True}),
+                "version_uuid": ("STRING", {"forceInput": True}),
+                "backend_api_host": ("STRING", {"forceInput": True}),
+                "backend_coginto_secret": ("STRING", {"forceInput": True}),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("model_url",)
+    FUNCTION = "execute"
+    CATEGORY = PLATFORM_IO_CAT
+    OUTPUT_NODE = True
+    DESCRIPTION = """Get the model url from the backend"""
+
+    def execute(self, model_uuid, version_uuid, backend_api_host, backend_coginto_secret):
+        def get_secret(session, secret_name, region_name="eu-west-1"):
+            client = session.client(service_name="secretsmanager", region_name=region_name)
+            try:
+                response = client.get_secret_value(SecretId=secret_name)
+                if "SecretString" in response:
+                    return json.loads(response["SecretString"])
+            except Exception as e:
+                print(f"Error getting secret: {e}")
+                raise
+
+        session = boto3.Session()
+        backend_cognito_secret = get_secret(session, backend_coginto_secret)
+        if backend_cognito_secret is None:
+            raise Exception(f"Backend Cognito Secret with name {backend_coginto_secret} is not found")
+
+        client_id = backend_cognito_secret["client_id"]
+        client_secret = backend_cognito_secret["client_secret"]
+        client_scope = backend_cognito_secret["scope"]
+        cognito_response = requests.post(
+            backend_cognito_secret["cognito_oauth_url"],
+            data=f"grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}&scope={client_scope}/read",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        print(cognito_response.json())
+
+        headers = {
+            "accept": "application/json",
+            "authorization": "Bearer {}".format(cognito_response.json()["access_token"]),
+        }
+
+        response = requests.get(
+            f"{backend_api_host}/model/{model_uuid}/version/{version_uuid}",
+            headers=headers,
+        )
+        return response.json()
