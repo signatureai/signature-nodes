9f8d1c0117d3672b3e1b2600c76506485adfee87
Afonso Miguel <afonso.miguel@signature.ai>
2025-04-08 13:08:02 +0100

    feat: create validation for unlinked nodes in workflow

--- nodes/web/quality_checks/main.js
+++ nodes/web/quality_checks/main.js
@@ -1,6 +1,13 @@
 import { bypassNodes } from "./workflow/bypass_nodes.js";
 import { checkNodeGroupPresence } from "./workflow/check_node_group_presence.js";
+import { checkUnlinkedNodes } from "./workflow/check_unlinked_nodes.js";
 import { findNodesWithRandomizedControlAfterGenerateWidget } from "./workflow/nodes_with_randomized_control_after_generate.js";
 import { showWarningDialog } from "./workflow/warning_dialogue.js";
 
-export { bypassNodes, checkNodeGroupPresence, findNodesWithRandomizedControlAfterGenerateWidget, showWarningDialog };
+export {
+  bypassNodes,
+  checkNodeGroupPresence,
+  checkUnlinkedNodes,
+  findNodesWithRandomizedControlAfterGenerateWidget,
+  showWarningDialog,
+};
